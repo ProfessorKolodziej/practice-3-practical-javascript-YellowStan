dc085ceaac7228349d6730e666cd15abe979159c
YellowStan <yellow@bu.edu>
2024-10-31 18:44:39 -0400

    Completed Practice 3

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,23 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+const toggledParagraph = document.querySelector(".original");
+
+function colorParagraph() {
+    toggledParagraph.classList.toggle("green");
+    toggledParagraph.classList.toggle("original");
+}
+
+function opaceParagraph() {
+    toggledParagraph.classList.toggle("disappear");
+    toggledParagraph.classList.toggle("original");
+}
+
+const colorButton = document.querySelector(".color");
+
+colorButton.addEventListener("click", colorParagraph);
+
+const opaceButton = document.querySelector(".size");
+
+opaceButton.addEventListener("click", opaceParagraph);
